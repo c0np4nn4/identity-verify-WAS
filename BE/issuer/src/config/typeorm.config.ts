47286f2e57e896124be6879bf6c37f170d93cc73
Lee-clipse <79911816+Lee-clipse@users.noreply.github.com>
2024-04-04 00:36:23 +0900

    feat: Issuer에 DB 추가 시도
    현재 포트 오류로 안되는 상황

--- BE/issuer/src/config/typeorm.config.ts
+++ BE/issuer/src/config/typeorm.config.ts
@@ -1,17 +1,17 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
-import { StudentKeyPairEntity } from 'src/entity/student-key-pair.entity';
+import { StudentKeyPairEntity } from '../entity/student-key-pair.entity';
 
 export const TypeormConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'mysql',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
+  host: 'mysql',
+  port: 3306,
   username: configService.get<string>('DB_USERNAME'),
   password: configService.get<string>('DB_PWD'),
   database: 'db',
   entities: [StudentKeyPairEntity],
-  synchronize: false,
-  logging: false,
+  synchronize: true,
+  logging: true,
 });
