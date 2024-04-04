450f0de51421ab2b917e16dc3cc6b2d85501f242
Lee-clipse <79911816+Lee-clipse@users.noreply.github.com>
2024-04-05 00:42:15 +0900

    feat: #11 Service - jwt 설정 추가

--- BE/service/src/app.module.ts
+++ BE/service/src/app.module.ts
@@ -3,6 +3,7 @@ import { ServiceAPIModule } from './service/service-api.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeormConfig } from './config/typeorm.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -19,6 +20,16 @@ import { TypeormConfig } from './config/typeorm.config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) =>
         TypeormConfig(configService),
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `90d`,
+        },
+      }),
     }),
   ],
   controllers: [],
--- BE/service/src/service/service-api.module.ts
+++ BE/service/src/service/service-api.module.ts
@@ -4,9 +4,10 @@ import { ServiceAPIService } from './service-api.service';
 import { HttpModule } from '@nestjs/axios';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entity/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [HttpModule, TypeOrmModule.forFeature([UserEntity])],
+  imports: [HttpModule, TypeOrmModule.forFeature([UserEntity]), JwtModule],
   controllers: [ServiceAPIController],
   providers: [ServiceAPIService],
 })
