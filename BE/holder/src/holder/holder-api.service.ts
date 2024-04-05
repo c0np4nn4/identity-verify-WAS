97d09c737aac51e885271d22fa52113a925002b7
이진재 <jamjam1208@naver.com>
2024-04-05 14:05:10 +0900

    feat: #12 이메일 인증 요청시 토큰 발급
    서버 거치지 않고 JWT 토큰으로 인증하기 위함

--- BE/holder/src/holder/holder-api.service.ts
+++ BE/holder/src/holder/holder-api.service.ts
@@ -6,6 +6,7 @@ import { lastValueFrom } from 'rxjs';
 import { UserVCDto } from '../dto/user-vc.dto';
 import { EmailSendCodeDto } from '../dto/email-send-code.dto';
 import { MailerService } from '@nestjs-modules/mailer';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class HolderAPIService {
@@ -13,6 +14,7 @@ export class HolderAPIService {
     private httpService: HttpService,
     private readonly configService: ConfigService,
     private readonly mailerService: MailerService,
+    private jwtService: JwtService,
   ) {}
 
   // Issuer 호출
@@ -40,7 +42,10 @@ export class HolderAPIService {
     if (!emailRegex.test(email)) {
       return { statusCode: 400, data: { message: 'Invalid Email' } };
     }
-    const code = Math.floor(Math.random() * 900000) + 100000;
+    let code = '';
+    for (let i = 0; i < 6; i++) {
+      code += Math.round(Math.random() * 9).toString();
+    }
 
     // TODO: 인증코드 저장 과정 필요 (FE로 토큰 발급?)
     await this.mailerService.sendMail({
@@ -50,8 +55,8 @@ export class HolderAPIService {
       context: { code },
       html: `<h1></h1>`,
     });
-
-    return { statusCode: 200 };
+    const token = this.generateEmailCodeToken(email, code);
+    return { statusCode: 200, data: { token } };
   }
 
   // Issuer 호출) 학생 email - 학번 매칭 여부 검증
@@ -62,5 +67,9 @@ export class HolderAPIService {
         .get(url, { params: { email, studentNumber } })
         .pipe(map((response) => response?.data)),
     );
+  }
+
+  generateEmailCodeToken(email: string, code: string) {
+    return this.jwtService.sign({ email, code });
   }
 }
