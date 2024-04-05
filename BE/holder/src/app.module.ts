a1e597b43e3a9c2119841f79d86f7faf6b840558
이진재 <jamjam1208@naver.com>
2024-04-05 14:04:39 +0900

    refactor: Holder - 환경변수 참조 관계 수정

--- BE/holder/src/app.module.ts
+++ BE/holder/src/app.module.ts
@@ -2,32 +2,25 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { HolderAPIModule } from './holder/holder-api.module';
 import { MailerModule } from '@nestjs-modules/mailer';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     HolderAPIModule,
     ConfigModule.forRoot({
-      isGlobal: true,
       envFilePath:
         process.env.NODE_ENV === 'test'
           ? './src/config/.test.env'
           : './src/config/.launch.env',
-    }),
-    MailerModule.forRootAsync({
-      imports: [],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        transport: {
-          host: 'smtp.gmail.com',
-          port: 587,
-          auth: {
-            user: 'jinjae781@gmail.com',
-            pass: configService.get<string>('MAIL_PASSWORD'),
-          },
-        },
-        defaults: {
-          from: `"no-reply" <noreply@gmail.com>`,
-        },
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_CREATE_USER_VC: Joi.string().required(),
+        API_GET_USER_MAJOR: Joi.string().required(),
+        API_GET_PROOF_VALUE: Joi.string().required(),
+        API_VERIFY_MAJOR_MATCH: Joi.string().required(),
+        MAIL_PASSWORD: Joi.string().required(),
+        MAIL_USER: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
       }),
     }),
   ],
--- BE/holder/src/holder/holder-api.module.ts
+++ BE/holder/src/holder/holder-api.module.ts
@@ -2,9 +2,41 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { HolderAPIController } from './holder-api.controller';
 import { HolderAPIService } from './holder-api.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    JwtModule.registerAsync({
+      imports: [],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '300s' },
+      }),
+    }),
+    MailerModule.forRootAsync({
+      imports: [],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          port: 587,
+          auth: {
+            // TODO: launch.env에 추가
+            user: configService.get<string>('MAIL_USER'),
+            pass: configService.get<string>('MAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `"no-reply" <noreply@gmail.com>`,
+        },
+      }),
+    }),
+  ],
   controllers: [HolderAPIController],
   providers: [HolderAPIService],
 })
