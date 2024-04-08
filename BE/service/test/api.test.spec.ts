23e41147ed363c7958f452208619fbf09b622572
이진재 <jamjam1208@naver.com>
2024-04-08 17:19:09 +0900

    modify: Service - 테스트 코드 mock DB 사용
    API 2개, 총 4개의 경우 Unit 테스트

--- BE/service/test/api.test.spec.ts
+++ BE/service/test/api.test.spec.ts
@@ -6,17 +6,41 @@ import { ServiceAPIService } from '../src/service/service-api.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { HttpModule } from '@nestjs/axios';
+import { hashSync } from 'bcryptjs';
 
-const mockPostRepository = () => ({
-  save: jest.fn().mockResolvedValue(undefined),
-  findOne: jest.fn().mockResolvedValue(undefined),
-  delete: jest.fn().mockResolvedValue(undefined),
-});
-type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+class MockRepository {
+  mockId = 'id';
+  mockNickname = 'nickname';
+  mockPassword = hashSync('pwd', 10);
+
+  setMockEntity() {
+    const userReoisitory: UserEntity = new UserEntity();
+    userReoisitory.id = this.mockId;
+    userReoisitory.nickname = this.mockNickname;
+    userReoisitory.password = this.mockPassword;
+    return userReoisitory;
+  }
+
+  async findOne(object: any) {
+    const id = object?.where?.id;
+    const nickname = object?.where?.nickname;
+    const mockEntity = this.setMockEntity();
+    if (mockEntity.id === id) {
+      return mockEntity;
+    }
+    if (mockEntity.nickname === nickname) {
+      return mockEntity;
+    }
+    return false;
+  }
+
+  async save(object: any) {
+    return true;
+  }
+}
 
 describe('ServiceAPIController (e2e)', () => {
   let serviceApiService: ServiceAPIService;
-  let userRepository: MockRepository<UserEntity>;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -40,45 +64,43 @@ describe('ServiceAPIController (e2e)', () => {
         ServiceAPIService,
         {
           provide: getRepositoryToken(UserEntity),
-          useValue: mockPostRepository(),
+          useClass: MockRepository,
         },
       ],
     }).compile();
 
     serviceApiService = moduleFixture.get<ServiceAPIService>(ServiceAPIService);
-    userRepository = moduleFixture.get<MockRepository<UserEntity>>(
-      getRepositoryToken(UserEntity),
-    );
   });
 
   it('Register User: Success', async () => {
-    const [pk, nickname, id, password] = [
-      'user_pk',
-      'test_name',
-      'test',
-      'pwd',
-    ];
+    const [id, nickname, password] = ['new_id', 'new_nickname', 'pwd'];
     const dto = { nickname, id, password };
     const res = await serviceApiService.registerUser(dto);
 
-    expect(userRepository.save).toHaveBeenCalledTimes(1);
-    expect(res.data).toHaveProperty('pk');
+    expect(res.statusCode).toEqual(200);
+  });
 
-    await userRepository.delete({ pk });
+  it('Register User: Duplicate Fail', async () => {
+    const [id, nickname, password] = ['id', 'nickname', 'pwd'];
+    const dto = { nickname, id, password };
+    const res = await serviceApiService.registerUser(dto);
+
+    expect(res.statusCode).toEqual(400);
   });
 
   it('Login User: Success', async () => {
-    const [id, password] = ['test', 'pwd'];
-    await userRepository.save({
-      pk: 'user_pk',
-      id,
-      password,
-    });
-
+    const [id, password] = ['id', 'pwd'];
     const dto = { id, password };
     const res = await serviceApiService.loginUser(dto);
-    console.log(res);
 
-    await userRepository.delete({ id });
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('Login User: 404 Fail', async () => {
+    const [id, password] = ['none_id', 'none_pwd'];
+    const dto = { id, password };
+    const res = await serviceApiService.loginUser(dto);
+
+    expect(res.statusCode).toEqual(404);
   });
 });
