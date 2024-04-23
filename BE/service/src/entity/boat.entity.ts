f1c14ad9d46f0657603364c2644b3a904aec3296
edac99 <jamjam1208@naver.com>
2024-04-23 22:18:18 +0900

    feat: mock data 생성 API 구현

--- BE/service/src/entity/boat.entity.ts
+++ BE/service/src/entity/boat.entity.ts
@@ -21,40 +21,40 @@ export class BoatEntity {
   })
   userPk: string;
 
-  @Column({ type: 'varchar', name: 'label_1' })
+  @Column({ type: 'varchar', name: 'label_1', nullable: true })
   label1: string;
 
-  @Column({ type: 'varchar', name: 'label_2' })
+  @Column({ type: 'varchar', name: 'label_2', nullable: true })
   label2: string;
 
-  @Column({ type: 'varchar', name: 'label_3' })
+  @Column({ type: 'varchar', name: 'label_3', nullable: true })
   label3: string;
 
-  @Column({ type: 'varchar', name: 'label_4' })
+  @Column({ type: 'varchar', name: 'label_4', nullable: true })
   label4: string;
 
-  @Column({ type: 'varchar', name: 'label_5' })
+  @Column({ type: 'varchar', name: 'label_5', nullable: true })
   label5: string;
 
-  @Column({ type: 'varchar', name: 'label_6' })
+  @Column({ type: 'varchar', name: 'label_6', nullable: true })
   label6: string;
 
-  @Column({ type: 'varchar', name: 'label_7' })
+  @Column({ type: 'varchar', name: 'label_7', nullable: true })
   label7: string;
 
-  @Column({ type: 'varchar', name: 'label_8' })
+  @Column({ type: 'varchar', name: 'label_8', nullable: true })
   label8: string;
 
-  @Column({ type: 'varchar', name: 'label_9' })
+  @Column({ type: 'varchar', name: 'label_9', nullable: true })
   label9: string;
 
-  @Column({ type: 'varchar', name: 'label_10' })
+  @Column({ type: 'varchar', name: 'label_10', nullable: true })
   label10: string;
 
-  @Column({ type: 'varchar', name: 'secrete_1' })
+  @Column({ type: 'varchar', name: 'secrete_1', nullable: true })
   secrete1: string;
 
-  @Column({ type: 'varchar', name: 'secrete_2' })
+  @Column({ type: 'varchar', name: 'secrete_2', nullable: true })
   secrete2: string;
 
   @Column({ type: 'boolean', name: 'is_occupied', default: false })
