dbc98758e41f09ed7237869f01df68e19d7f850d
edac99 <jamjam1208@naver.com>
2024-05-01 14:26:40 +0900

    test: count 증가 테스트 구현

--- BE/issuer/test/api.test.spec.ts
+++ BE/issuer/test/api.test.spec.ts
@@ -1,6 +1,40 @@
+/* eslint-disable prefer-const */
 import { Test, TestingModule } from '@nestjs/testing';
 import { IssuerAPIService } from '../src/issuer/issuer-api.service';
 import { ConfigModule } from '@nestjs/config';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CounterEntity } from '../src/entity/counter.entity';
+
+class CounterRepository {
+  setMockRepository() {
+    const counterReoisitory: CounterEntity[] = [];
+    let row: CounterEntity;
+
+    row = new CounterEntity();
+    row.id = 'counter';
+    row.count = 0;
+    counterReoisitory.push(row);
+    return counterReoisitory;
+  }
+
+  async findOne(object: any) {
+    const id = object?.where?.id;
+
+    const mockRepository = this.setMockRepository();
+    let result: CounterEntity;
+    mockRepository.map((row) => {
+      if (id === 'counter') {
+        result = row;
+        return;
+      }
+    });
+    return result;
+  }
+
+  async save(object: any) {
+    return this.setMockRepository();
+  }
+}
 
 describe('IssuerAPIController (e2e)', () => {
   // 블록체인 적재시 걸리는 시간 고려
@@ -16,7 +50,13 @@ describe('IssuerAPIController (e2e)', () => {
           isGlobal: true,
         }),
       ],
-      providers: [IssuerAPIService],
+      providers: [
+        IssuerAPIService,
+        {
+          provide: getRepositoryToken(CounterEntity),
+          useClass: CounterRepository,
+        },
+      ],
     }).compile();
 
     issuerApiService = moduleFixture.get<IssuerAPIService>(IssuerAPIService);
@@ -29,17 +69,33 @@ describe('IssuerAPIController (e2e)', () => {
     expect(res).toHaveProperty('vc');
   });
 
-  //! {"index":0,"kind":{"index":0,"kind":{"FunctionCallError":{"ExecutionError":"The length of a log message 16402 exceeds the limit 16384"}}}}
-  // it('Load Key Chain: Success', async () => {
-  //   const issuerPubKey = 'quixotic-debt.testnet';
-  //   const vc =
-  //     '{"context":["https://www.w3.org/ns/credentials/v2"],"id":"url:uuid:21de72f6-0f4a-4e59-8beb-73a17a863f9c","credential_type":["VerifiableCredential","MajorCredential"],"issuer":"did:near:pnu.testnet","validFrom":"2024-04-07T14:17:17Z","credentialSubject":{"id":"did:near:123123.testnet","subject":{"school_did":"did:near:pnu.testnet","major":"24"}},"proof":{"type":"CircRefNEARDIDProof","cryptosuite":"eddsa","created":"2024-04-07T14:17:17Z","verificationMethod":"CircRefVCSignatureVerificationMethod","proofPurpose":"assertionMethod","proofValue":"1B59B0290FEA0A7C8EB3308FA5AE87AFCC970D5C68AE651CE8E7A002E121A993F5EF3FCDEDC9C4A64E76A119F42259D2B4F2D24999469871CB2288A5E9C39402"}}';
-  //   const res = await issuerApiService.loadKeyChain(issuerPubKey, vc);
-  //   expect(Array.isArray(res)).toBe(true);
-  // });
+  it('Load Key Chain: Success', async () => {
+    const issuerPubKey = 'goofy-stone.testnet';
+    const vc = `{
+        uuid: '505264c8-fea3-4ff0-8bb4-d5b49ea7936b',
+        vc: {
+          context: [ 'https://www.w3.org/ns/credentials/v2' ],
+          id: 'url:uuid:505264c8-fea3-4ff0-8bb4-d5b49ea7936b',
+          credential_type: [ 'VerifiableCredential', 'MajorCredential' ],
+          issuer: 'did:near:pnu.testnet',
+          validFrom: '2024-05-01T13:45:03Z',
+          credentialSubject: { id: 'did:near:hpubkey.testnet', subject: [Object] },
+          proof: {
+            type: 'CircRefNEARDIDProof',
+            cryptosuite: 'eddsa',
+            created: '2024-05-01T13:45:03Z',
+            verificationMethod: 'CircRefVCSignatureVerificationMethod',
+            proofPurpose: 'assertionMethod',
+            proofValue: ''
+          }
+        }
+      }`;
+    const res = await issuerApiService.loadKeyChain(issuerPubKey, vc);
+    expect(Array.isArray(res)).toBe(true);
+  });
 
-  it('Generate Proof Value: Success', () => {
-    const res = issuerApiService.generateProofValue();
+  it('Generate Proof Value: Success', async () => {
+    const res = await issuerApiService.generateProofValue();
     expect(res).toHaveProperty('proofValue');
     expect(res).toHaveProperty('message');
   });
